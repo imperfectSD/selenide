049d22c092b5bdb4487a487da9094affe927a88a
imperfectSD <imperfect.sparda@gmail.com>
2021-11-03 11:40:49 +0300

    fixes

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -2,19 +2,21 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 class CardOrderTest {
 
-    String Date(int date) {
+    String generateDate(int date) {
         return LocalDate.now().plusDays(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
@@ -22,12 +24,13 @@ class CardOrderTest {
     void cardWithDelivery() {
         open("http://localhost:9999");
         $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").clear();
-        $("[placeholder='Дата встречи']").setValue(Date(3));
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(generateDate(4));
         $("[name='name']").setValue("Иванов Иван");
         $("[name='phone']").setValue("+79999999999");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(withText("Встреча успешно забронирована")).shouldHave(text(generateDate(4)));
     }
 }
